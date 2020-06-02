8a36f3fb49c86ea6d187b7286534285f33a01fc6
ZannWolf <66300411+ZannWolf@users.noreply.github.com>
2020-06-02 11:22:24 +0300

    Update Go.py

--- Go.py
+++ Go.py
@@ -9,8 +9,8 @@ class MyClient(discord.Client):
 		print('Бот подключился - {0}!'.format(self.user))
 		
 	async def on_member_join(self, member):
-    		role = discord.utils.get(member.guild.roles, id=707936779939872770)
-       		await member.add_roles(role)
+		role = discord.utils.get(member.guild.roles, id=707936779939872770)
+		await member.add_roles(role)
 	
 	async def on_raw_reaction_add(self, payload):
 		if payload.message_id == config.POST_ID:
