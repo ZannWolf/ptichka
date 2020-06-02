6d4f32c82f1c0f8566a72f5197ba7a8c6cb02b5b
ZannWolf <66300411+ZannWolf@users.noreply.github.com>
2020-06-02 10:54:50 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,16 @@
+
+
+POST_ID = 717274322699288646 #id soocsheniy
+
+ROLES = {
+         '🐦':717269588399882240, #neob
+         '🎨':717037346629091328, #hyd
+         '🎸':717037567677169734, #mus
+         '🎤':717037570151809045, #yout
+         '💻':717269428429127724, #devel
+
+} #roli
+
+EXCROLES = () #eskluchenie
+
+MAX_ROLES_PER_USER = 5 #collichestvo
